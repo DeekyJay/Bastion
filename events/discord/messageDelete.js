bc04a11aa96d5d33c6db5e5ff56fa2b1ae064cab
Derek Jensen <derek@deek.io>
2020-09-10 19:59:28 -0700

    show contents of deleted message

--- events/discord/messageDelete.js
+++ events/discord/messageDelete.js
@@ -4,15 +4,15 @@
  * @license GPL-3.0
  */
 
-module.exports = async message => {
+module.exports = async (message) => {
   try {
     if (!message.guild) return;
 
     let guildModel = await message.client.database.models.guild.findOne({
-      attributes: [ 'serverLog' ],
+      attributes: ['serverLog'],
       where: {
-        guildID: message.guild.id
-      }
+        guildID: message.guild.id,
+      },
     });
 
     if (!guildModel || !guildModel.dataValues.serverLog) return;
@@ -20,49 +20,50 @@ module.exports = async message => {
     let logChannel = message.guild.channels.get(guildModel.dataValues.serverLog);
     if (!logChannel) return;
 
-    logChannel.send({
-      embed: {
-        color: message.client.colors.RED,
-        title: message.client.i18n.event(message.guild.language, 'messageDelete'),
-        fields: [
-          {
-            name: 'Message Channel',
-            value: message.channel.toString(),
-            inline: true
-          },
-          {
-            name: 'Message Channel ID',
-            value: message.channel.id,
-            inline: true
-          },
-          {
-            name: 'Message ID',
-            value: message.id,
-            inline: true
-          },
-          {
-            name: 'Message Author',
-            value: message.author.tag,
-            inline: true
-          },
-          {
-            name: 'Message Author ID',
-            value: message.author.id,
-            inline: true
-          },
-          {
-            name: 'Message Content',
-            value: '`[REDACTED]`',
-            inline: true
-          }
-        ],
-        timestamp: new Date()
-      }
-    }).catch(e => {
-      message.client.log.error(e);
-    });
-  }
-  catch (e) {
+    logChannel
+      .send({
+        embed: {
+          color: message.client.colors.RED,
+          title: message.client.i18n.event(message.guild.language, 'messageDelete'),
+          fields: [
+            {
+              name: 'Message Channel',
+              value: message.channel.toString(),
+              inline: true,
+            },
+            {
+              name: 'Message Channel ID',
+              value: message.channel.id,
+              inline: true,
+            },
+            {
+              name: 'Message ID',
+              value: message.id,
+              inline: true,
+            },
+            {
+              name: 'Message Author',
+              value: message.author.tag,
+              inline: true,
+            },
+            {
+              name: 'Message Author ID',
+              value: message.author.id,
+              inline: true,
+            },
+            {
+              name: 'Message Content',
+              value: message.content,
+              inline: true,
+            },
+          ],
+          timestamp: new Date(),
+        },
+      })
+      .catch((e) => {
+        message.client.log.error(e);
+      });
+  } catch (e) {
     message.client.log.error(e);
   }
 };
